00002ac8ca11d43575e712946a34bd082b200473
Данияр Габдеев <daniargabdeev@gmail.com>
2021-10-14 11:42:00 +0300

    тест

---
+++ pz_8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i <= 9; i++)//Вычесление и запсись элементов массмва
+                Console.WriteLine($"{rnd.Next(0,5)}");//Вывод элементов массива
+        }
+    }
+}
