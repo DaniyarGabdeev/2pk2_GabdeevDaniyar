1930db500ef0f62897bb44dd3c97d4fb58e98b46
Данияр Габдеев <daniargabdeev@gmail.com>
2022-05-03 12:30:17 +0300

    t

---
+++ ПЗ_11.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ПЗ_11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int* p;
+            p = &num;
+            char** c;
+            c = (char**)&p;
+            *(*c) = '*';
+            c[0][1] = '+';
+            for (int k = 0; k < 2; k++)
+            Console.WriteLine($"значение *: {*(*c)}");
+            Console.WriteLine($"значение +: { c[0][1]}");
+        }
+    }
+}
